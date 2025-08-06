3c112253f13e24edb97f7b6c5c4ddbb1259aeec7
Leilasempere <leilasempere@gmail.com>
2025-08-06 15:17:25 +0200

    ratelimiter

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoute.js';
+import { authLimiter } from './middlewares/rateLimiter.js';
 
 dotenv.config();
 const app = express();
@@ -9,9 +10,13 @@ app.set("view engine", "twig")
 
 app.use(express.json());
 
+app.use( authLimiter); 
 app.use('/api/user', userRoutes);
+
 
 const PORT = process.env.PORT || 5000  ;
 app.listen(PORT, () => {
   console.log(` Serveur démarré sur http://localhost:${PORT}`);
 });
+
+
