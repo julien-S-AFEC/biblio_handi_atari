b395c1a3a47b0832220d7d65b6711d173572caef
Alejandro <alesm91@hotmail.com>
2025-08-06 13:57:16 +0200

    modif de documents pour bien lister tous

--- controller/documentController.js
+++ controller/documentController.js
@@ -2,7 +2,8 @@ import { Document } from '../models/documentModel.js';
 
 export async function listDocuments(req, res) {
     try {
-        const [documents] = await Document.listDocuments();
+        const documents = await Document.listDocuments();
+        
         res.status(200).json(documents);
         // res.render(`documents`, {documents});
     } catch (error) {
@@ -14,12 +15,13 @@ export async function listDocuments(req, res) {
 export async function getDocumentById(req, res) {
     const { id } = req.params;
     try {
-        const [rows] = await Document.findDocumentById(id);
-        if (!rows.length) return res.status(404).json({ error: "Catégorie non trouvée" });
-        res.json(rows[0]);
+        const document = await Document.findDocumentById(id);
+        
+        if (!document) return res.status(404).json({ error: "Document non trouvée" });
+        res.status(200).json(document);
     } catch (err) {
-        console.error(err);  
-        res.status(500).send('Erreur serveur')
+        console.error(err);
+        res.status(500).send('Erreur serveur');
     }
 }
 
