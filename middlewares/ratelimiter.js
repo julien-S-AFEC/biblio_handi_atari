cf68a6ba821c08fcfba9520fc7b6362d088d3352
Leilasempere <leilasempere@gmail.com>
2025-08-06 16:38:55 +0200

    securité2

--- middlewares/ratelimiter.js
+++ middlewares/ratelimiter.js
@@ -2,11 +2,11 @@
 import rateLimit from 'express-rate-limit';
 
 export const authLimiter = rateLimit({
-  windowMs: 2 * 60 * 1000, // 15 minutes
+  windowMs: 2 * 60 * 1000, // 2 minutes
   limit: 5, // Limite à 5 tentatives de login par IP
   standardHeaders: 'draft-8',
   legacyHeaders: false,
   message: {
-    message: "Trop de tentatives de connexion. Réessayez dans 15 minutes.",
+    message: "Trop de tentatives de connexion. Réessayez dans 2 minutes.",
   },
 });
