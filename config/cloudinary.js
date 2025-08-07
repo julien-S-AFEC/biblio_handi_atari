98c6a71a82984e978ab0caa35ae2c655b9bdd630
Soutade Julien <julien.soutade.TD@gmail.com>
2025-08-07 12:01:41 +0200

    Test

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -8,16 +8,6 @@ cloudinary.v2.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-import multer from 'multer'
-import cloudinary from 'cloudinary'
-import { CloudinaryStorage } from 'multer-storage-cloudinary'
-
-cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-});
-
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary.v2,
 
